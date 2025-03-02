922be01024e814f7d0e73e1beef8dd899708f0ca
Kashan Shaikh <shaikhkashan670@gmail.com>
2025-03-02 17:41:00 +0500

    upload

---
+++ resources/views/admin_panel/include/admin_sidebar_include.blade.php
@@ -0,0 +1,87 @@
+<aside
+    id="layout-menu"
+    class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="#" class="app-brand-link">
+            <img src="logo_white.png" style="width: 120px;" alt="">
+        </a>
+
+        <a
+            href="javascript:void(0);"
+            class="layout-menu-toggle menu-link text-large ms-auto">
+            <i
+                class="ti menu-toggle-icon d-none d-xl-block ti-sm align-middle"></i>
+            <i class="ti ti-x d-block d-xl-none ti-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboards -->
+        <li class="menu-item active open">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-smart-home"></i>
+                <div data-i18n="Dashboards">Dashboards</div>
+            </a>
+        </li>
+        <!-- Front Pages -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons ti ti-files"></i>
+                <div data-i18n="Front Pages">Front Pages</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a
+                        href="https://demos.pixinvent.com/vuexy-html-admin-template/html/front-pages/landing-page.html"
+                        class="menu-link"
+                        target="_blank">
+                        <div data-i18n="Landing">Landing</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a
+                        href="https://demos.pixinvent.com/vuexy-html-admin-template/html/front-pages/pricing-page.html"
+                        class="menu-link"
+                        target="_blank">
+                        <div data-i18n="Pricing">Pricing</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a
+                        href="https://demos.pixinvent.com/vuexy-html-admin-template/html/front-pages/payment-page.html"
+                        class="menu-link"
+                        target="_blank">
+                        <div data-i18n="Payment">Payment</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a
+                        href="https://demos.pixinvent.com/vuexy-html-admin-template/html/front-pages/checkout-page.html"
+                        class="menu-link"
+                        target="_blank">
+                        <div data-i18n="Checkout">Checkout</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a
+                        href="https://demos.pixinvent.com/vuexy-html-admin-template/html/front-pages/help-center-landing.html"
+                        class="menu-link"
+                        target="_blank">
+                        <div data-i18n="Help Center">Help Center</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <!-- Apps & Pages -->
+
+        <li class="menu-item">
+            <a href="app-email.html" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-mail"></i>
+                <div data-i18n="Email">Email</div>
+            </a>
+        </li>
+    </ul>
+</aside>
---
+++ resources/views/admin_panel/include/header_include.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html
+    lang="en"
+    class="light-style layout-navbar-fixed layout-menu-fixed layout-compact"
+    dir="ltr"
+    data-theme="theme-bordered"
+    data-assets-path="assets/"
+    data-template="vertical-menu-template-bordered">
+
+<head>
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Dashboard - Analytics | Vuexy - Bootstrap Admin Template</title>
+
+    <meta
+        name="description"
+        content="Start your development with a Dashboard for Bootstrap 5" />
+    <meta
+        name="keywords"
+        content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;ampdisplay=swap"
+        rel="stylesheet" />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="assets/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="assets/vendor/fonts/tabler-icons.css" />
+    <link rel="stylesheet" href="assets/vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="assets/vendor/css/rtl/core.css" />
+    <link rel="stylesheet" href="assets/vendor/css/rtl/theme-bordered.css" />
+    <link rel="stylesheet" href="assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/node-waves/node-waves.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/typeahead-js/typeahead.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="assets/vendor/libs/swiper/swiper.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+    <link
+        rel="stylesheet"
+        href="assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
+
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="assets/vendor/css/pages/cards-advance.css" />
+
+    <!-- Helpers -->
+    <script src="assets/vendor/js/helpers.js"></script>
+    <script src="assets/vendor/js/template-customizer.js"></script>
+    <script src="assets/js/config.js"></script>
+</head>
+
+<body>
+    <noscript><iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP"
+            height="0"
+            width="0"
+            style="display: none; visibility: hidden"></iframe></noscript>
